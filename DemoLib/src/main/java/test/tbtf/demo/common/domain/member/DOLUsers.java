45ca5d944dc44bd66b9ad8060a0d31ca8e00d728
tbtfcode <tbtf.base.code@gmail.com>
2014-11-27 17:24:08 +0900

    Default WAS Setting.

---
+++ DemoLib/src/main/java/test/tbtf/demo/common/domain/member/DOLUsers.java
@@ -0,0 +1,52 @@
+/**
+ * 
+ */
+package test.tbtf.demo.common.domain.member;
+
+import test.tbtf.demo.common.domain.DOLBaseDomain;
+
+/**
+ * @project DemoLib
+ * @package test.tbtf.demo.common.domain.member
+ * @file DOLUsers.java
+ * @date 2014. 11. 13.
+ * @author tbtf.base.code@gmail.com
+ * @description
+ */
+public class DOLUsers extends DOLBaseDomain {
+
+	private static final long serialVersionUID = -3553843079369040628L;
+
+	private String userId;
+
+	private String userName;
+
+	/**
+	 * @return the userId
+	 */
+	public String getUserId() {
+		return userId;
+	}
+
+	/**
+	 * @param userId the userId to set
+	 */
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	/**
+	 * @return the userName
+	 */
+	public String getUserName() {
+		return userName;
+	}
+
+	/**
+	 * @param userName the userName to set
+	 */
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+}
